342ea7decf8e92526410721ea806bf06e9202c1c
Yu-Gi-Oh! Holic TV 개인계정 <leee4566@gmail.com>
2022-04-29 00:57:08 +0000

    Projectile

---
+++ unity_game/Projectile.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 발사체에 부딪힌 오브젝트의 태그가 "Enemy"이면
+        if (collision.CompareTag("Enemy"))
+        {
+            // 부딪힌 오브젝트 사망처리 (적)
+            Destroy(collision.gameObject);
+            // 내 오브젝트 삭제 (발사체)
+            Destroy(gameObject);
+        }
+    }
+}
+
+/*
+ * File : Projectile.cs
+ * Desc
+ *  : 플레이어 캐릭터의 공격 발사체
+ *  
+ * Functisons
+ *  : OnTriggerEnter2D() - 적과 충돌했을 때 처리
+ */

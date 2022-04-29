df39f2a0dd2da0a759576f6ae1fb6a934ed49c3d
Yu-Gi-Oh! Holic TV 개인계정 <leee4566@gmail.com>
2022-04-29 00:34:49 +0000

    Metworite

---
+++ unity_game/Metworite.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Metworite : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 운석에 부딪힌 오브젝트의 태그가 "Playe"이면  
+        if (collision.CompareTag("Player"))
+        {
+            // 운석 사망
+            Destroy(gameObject);
+        }
+    }
+}
+/*
+ * File : Metworite.cs
+ * Desc
+ *  : 운석 오브젝트에 부착해서 사용
+ *  
+ * Functions
+ * 
+ */

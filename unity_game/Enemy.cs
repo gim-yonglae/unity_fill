5ec2469b95a661709fc235e9e57a4817f1622f43
Yu-Gi-Oh! Holic TV 개인계정 <leee4566@gmail.com>
2022-05-03 03:10:35 +0000

    Enemy

--- unity_game/Enemy.cs
+++ unity_game/Enemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    private int damage;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // 적에게 부딪힌 오브젝트의 태그가 "Player"이면

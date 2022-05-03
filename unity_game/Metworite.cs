6d1d52610ca394ff96b3073b5c36aa565b67d061
Yu-Gi-Oh! Holic TV 개인계정 <leee4566@gmail.com>
2022-05-03 04:32:52 +0000

    Metworite

--- unity_game/Metworite.cs
+++ unity_game/Metworite.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Metworite : MonoBehaviour
 {
+    [SerializeField]
+    private int damage = 5; //운석 공격력
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // 운석에 부딪힌 오브젝트의 태그가 "Playe"이면  
         if (collision.CompareTag("Player"))
         {
+            // 운석 공격력만큼 플레이어 체력감소
+            collision.GetComponent<PlayerHP>().TakeDamage(damage);
             // 운석 사망
             Destroy(gameObject);
         }

581582be8cf56b9ed40865f6d3a4cf9a2490a168
Yu-Gi-Oh! Holic TV 개인계정 <leee4566@gmail.com>
2022-05-03 03:25:40 +0000

    Enemy

--- unity_game/Enemy.cs
+++ unity_game/Enemy.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField]
-    private int damage = 1;
+    private int damage = 1; // 적 공격력
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // 적 공격력만큼 플레이어 체력 감소
+            collision.GetComponent<PlayerHP>().TakeDamag(damage);
         // 적에게 부딪힌 오브젝트의 태그가 "Player"이면
         if (collision.CompareTag("Player"))
         {
